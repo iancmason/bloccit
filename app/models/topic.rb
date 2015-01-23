237c5756a3e75b10c93d5060561f6c81a0863e13
vagrant <ian.christopher.mason@gmail.com>
2015-01-23 02:40:10 +0000

    Topic Spec

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,14 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
     # scope :visible_to, -> { where(public: true) }
-    scope :visible_to, -> (user) { user ? all : where(public: true) }
+    scope :visible_to, -> (user) { user ? all : where(public: true) } 
+
+    scope :publicly_viewable,  -> {where(public: true)}
+    scope :privately_viewable, -> {where(public: false)}
+
+
+
+    # def self.publicly_viewable
+    #   Topic.where(public: true)
+    # end
 end
