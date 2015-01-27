513f65bf3715a115d0dd9e57c848703794d89a7e
vagrant <ian.christopher.mason@gmail.com>
2015-01-27 23:09:35 +0000

    Creating Comments with Ajax added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,40 +1,56 @@
-class CommentsController < ApplicationController
+ class CommentsController < ApplicationController
   respond_to :html, :js
 
   def create
-    @topic = Topic.find(params[:topic_id])
+    #@topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
     @comments = @post.comments
 
-
-    @comment = current_user.comments.build(params[:comment].permit!)
+    @comment = current_user.comments.build( comment_params )
     @comment.post = @post
+    @new_comment = Comment.new
 
-    # authorize! :create, @comment, message: "Sign in to make comment buddy"
+    authorize @comment
+
     if @comment.save
-      flash[:notice] = "Comment has been added."
-      redirect_to [@topic, @post]
+      flash[:notice] = "Comment was created."
     else
-      flash[:error] = "Error creating comment. Please try again."
-      render 'posts/show'
+      flash[:error] = "There was an error. Please try again."
+    end
+
+    respond_with(@comment) do |f|
+      f.html { redirect_to [@topic, @post] }
     end
   end
 
+  def show
+    @topic = Topic.find(params[:id])
+    authorize @topic
+    @posts = @topic.posts.includes(:user).includes(:comments).paginate(page: params[:page], per_page: 10)
+  end
+
   def destroy
-     @topic = Topic.find(params[:topic_id])
-     @post = @topic.posts.find(params[:post_id])
-     @comment = @post.comments.find(params[:id])
- 
-     authorize @comment
-     if @comment.destroy
-       flash[:notice] = "Comment was removed."
-       #redirect_to [@topic, @post]
-     else
-       flash[:error] = "Comment couldn't be deleted. Try again."
-       #redirect_to [@topic, @post]
-     end
-       respond_with(@comment) do |format|
-       format.html { redirect_to [@post.topic, @post] }
-       end
-   end
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
+    authorize @comment
+    if @comment.destroy
+      flash[:notice] = "Comment was removed."
+    else
+      flash[:error] = "Comment was not deleted. Try again."
+    end
+
+    respond_with(@comment) do |f|
+      f.html { redirect_to [@topic, @post] }
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :post_id)
+  end
+
 end
+
